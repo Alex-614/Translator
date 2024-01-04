b15d3e00a6ff73ed68189776bbf3dce76fff6c07
Daezu <niklas.daezu@gmail.com>
2024-01-04 19:23:44 +0100

    docker update

--- translation/translation.py
+++ translation/translation.py
@@ -1,5 +1,8 @@
 
-import libretranslate, logging, os
+import logging, os
+from aiohttp import web
+import aiohttp_cors
+import translate
 
 debugmode = os.environ.get('TRANSCRIPTION_DEBUGMODE', False)
 
@@ -13,10 +16,38 @@ log.addHandler(ch)
 log.setLevel(loglevel)
 
 log.info("starting translation service...")
-#
-# starts Libretranslate
-#
-libretranslate.main()
+
+async def doTranslate(request):
+    params = await request.post()
+    log.debug("received translate request: q='" + params.get("q") + "' source='" + params.get("source") + "' target='" + params.get("target") + "'")
+    #{"q": q, "source": source, "target": target}
+    translator = translate.Translator(from_lang = params.get("source"), to_lang = params.get("target"))
+    translated = translator.translate(params.get("q"))
+    log.debug("translated: " + translated)
+    return web.Response(
+            content_type='application/json',
+            text='{"translatedText": "' + translated + '"}')
+
+
+if __name__ == '__main__':
+
+    app = web.Application()
+
+    app._router.add_post("/translate", doTranslate)
+    
+    cors = aiohttp_cors.setup(app, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*"
+        )
+    })
+
+    for route in list(app.router.routes()):
+        cors.add(route)
+
+    web.run_app(app, port=5000, ssl_context=None)
+
 log.info("translation service stopped")
 
 
