27cc584d33cfb714cb3868f92aa4d802dfc94203
Xarristo <115558698+Xarristo@users.noreply.github.com>
2024-01-16 11:38:44 +0100

    Changed length of text_line varchar

--- Database/Dockerimages/TextDBImage/Leader/init.sql
+++ Database/Dockerimages/TextDBImage/Leader/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "text" (
     session_id VARCHAR(36),
     timestamp TIMESTAMP,
-    text_line VARCHAR(255) NOT NULL,
+    text_line VARCHAR(65535) NOT NULL,
     PRIMARY KEY (session_id, timestamp)
 );
